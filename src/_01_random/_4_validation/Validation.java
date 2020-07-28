7b7b9018734fa701908e714707a980b65a1e3f93
root <root@8d21d1874d85>
2020-07-28 23:59:05 +0000

    Magic 8 ball

--- src/_01_random/_4_validation/Validation.java
+++ src/_01_random/_4_validation/Validation.java
@@ -11,15 +11,38 @@ public class Validation {
 	public static void main(String[] args) {
 		
 		Random randomMaker = new Random();
+		for (int i = 0; i < 10; i++) {
+			
 		
-		int randomNumber = randomMaker.nextInt(5);
+		int randomNumber = randomMaker.nextInt(6);
 		
 		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
-
+		if (randomNumber == 0) {
+			JOptionPane.showMessageDialog(null, "Good job with working on this java code!");
+		}
+		else if (randomNumber==1) {
+			JOptionPane.showMessageDialog(null, "I like your color choice ");
+		}
+		
+		else if (randomNumber==2) {
+			JOptionPane.showMessageDialog(null, "Roses are red, Violets and blue, Sugar is sweet and so are you! :)");
+		}	
+		
+		else if (randomNumber==3) {
+			JOptionPane.showMessageDialog(null, " Nice job!!! "); 
+		}
+		else if (randomNumber==4) {
+			JOptionPane.showMessageDialog(null, "Good things will happen in the future ");
+		}
+		
+		else if (randomNumber==5) {
+			JOptionPane.showMessageDialog(null, "I like your shirt");
+		}
 		// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program. They will like it :)
 	}
+}
 }
--- src/_01_random/_5_magic_8_ball/Magic8Ball.java
+++ src/_01_random/_5_magic_8_ball/Magic8Ball.java
@@ -6,32 +6,49 @@ package _01_random._5_magic_8_ball;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Magic8Ball {
 
 	// 1. Make a main method that includes all the steps below
+public static void main(String[] args) {
+	
 
 	// 2. Get the user to enter a question for the 8 ball to answer
-	
+	JOptionPane.showInputDialog("What is your question for the 8ball?");
 
 	// 3. Make a variable and initialize it to a random number.
 	//     ** You will need to make a random object!
+	Random ran=new Random();
 	//     Limit the random numbers to be between 0 and 3
+int number=ran.nextInt(4); 
+	
 
 	
 	// 4. If the random number is 0
+if (number==0){
+	JOptionPane.showInputDialog("Yes");
 
+}
 	// -- tell the user "Yes"
 
 	// 5. If the random number is 1
+else if (number==1){
+	JOptionPane.showInputDialog("No");
 
+}
 	// -- tell the user "No"
 
 	// 6. If the random number is 2
+if (number==2){
+	JOptionPane.showInputDialog("Maybe you should ask Google");
 
+}
 	// -- tell the user "Maybe you should ask Google?"
 
 	// 7. If the random number is 3
 
 	// -- write your own answer
 
+}
 }
